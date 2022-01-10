b06a503b96e09600d36832a2a3c753d793f95a22
Lee Macri <lem@rathkeale.school.nz>
2022-01-11 10:32:34 +1300

    Start activity added

---
+++ starter_activity_sheet_1.py
@@ -0,0 +1,11 @@
+# starter program lesson 1
+
+name = "Billy"
+print("We want to know if you like progamming!")
+print()
+print("Do you like programming " + name + "?")
+answer = input()
+print("Great! You said " + answer + "!")
+print("Let's learn some Python today")
+
+
