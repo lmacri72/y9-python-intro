5e805b940af7d82408dafe3b7a5853550b66c631
Lee Macri <lem@rathkeale.school.nz>
2022-01-26 09:22:48 +1300

    Add Activity 3

---
+++ starter_activity_sheet_3.py
@@ -0,0 +1,24 @@
+# starter program week 3 - perhaps this could be a task
+
+
+def cooking():
+    print("Meal planner")
+    print()
+    print("1. Chicken curry ")
+    print("2. Veggie lasagne")
+    print("3. Burger and salad")
+    print()
+    print("Which of these meals is your favourite? (1, 2 or 3) ")
+    answer = input()
+    if answer == "1":
+        print("Chicken curry coming up")
+    elif answer == "2":
+        print("Veggie lasagne coming up")
+    else:
+        print("Burger and salad coming up!")
+    print("Enjoy!")
+    
+     
+    
+cooking()
+
