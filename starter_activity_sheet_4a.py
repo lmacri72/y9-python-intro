c5f65b22d236915e321b39160d69e514c477d95a
Lee Macri <lem@rathkeale.school.nz>
2022-02-12 15:08:04 +1300

    Add activity 4a

---
+++ starter_activity_sheet_4a.py
@@ -0,0 +1,26 @@
+# program written for Session 4
+# Anything written after # is ignored by the computer
+# We can use it to annotate programs to make them more readable
+
+from turtle import *
+
+# this line says we are using the "turtle" library of functions
+# remember to use this for turtle graphics in Python
+
+
+def mystery():
+    fillcolor("Green")
+    begin_fill()
+    pencolor("Red")
+    forward (100) 
+    right (90)
+    end_fill()
+    forward (100) 
+    right (90) 
+    forward (100) 
+    right (90) 
+    forward (100) 
+
+
+
+mystery()
