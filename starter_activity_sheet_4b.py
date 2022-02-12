851fbb1fe3ae53047e252f3a9411d3e01cc323dd
Lee Macri <lem@rathkeale.school.nz>
2022-02-12 16:58:04 +1300

    Update Turtle import

--- starter_activity_sheet_4b.py
+++ starter_activity_sheet_4b.py
@@ -1,12 +1,16 @@
 # week 4 Loops
 
-from turtle import *
+import turtle
 
+# Initializing the turtle
+t = turtle.Turtle()
+    
 def square():
+    
     for counter in range(4):
-        forward(100)
-        right(90)
+        t.forward(100)
+        t.right(90)
 
 square()
-left(45)
+t.left(45)
 square()
