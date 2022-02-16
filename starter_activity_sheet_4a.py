e86a323235cf2a98d4df4b00b425da2458d90733
Lee Macri <lem@rathkeale.school.nz>
2022-02-17 11:10:03 +1300

    Update starter_activity_sheet_4a.py

--- starter_activity_sheet_4a.py
+++ starter_activity_sheet_4a.py
@@ -2,23 +2,24 @@
 # Anything written after # is ignored by the computer
 # We can use it to annotate programs to make them more readable
 
-import turtle
+from turtle import *
+
 # this line says we are using the "turtle" library of functions
 # remember to use this for turtle graphics in Python
 
 
 def mystery():
-    t = turtle.Turtle()
-    t.fillcolor("Green")
-    t.begin_fill()
-    t.pencolor("Red")
-    t.forward (100) 
-    t.right (90)
-    t.end_fill()
-    t.forward (100) 
-    t.right (90) 
-    t.forward (100) 
-    t.right (90) 
-    t.forward (100) 
+    color('green', 'red')
+    begin_fill()
+    forward (100) 
+    right (90)
+    forward (100) 
+    right (90) 
+    forward (100) 
+    right (90) 
+    forward (100)
+    right (90)
+    end_fill()
+    done()
 
 mystery()
