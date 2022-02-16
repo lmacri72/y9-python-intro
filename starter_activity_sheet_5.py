a9a053bf942d7d44d6b37f912b26b87f7ced34f7
Lee Macri <lem@rathkeale.school.nz>
2022-02-16 13:52:14 +1300

    Create starter_activity_sheet_5.py

---
+++ starter_activity_sheet_5.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+## Task 1: Look at the following code and decide which picture it will generate.
+# Then download it from the shared drive, then save and run it, to check.
+
+def demo():
+  pencolor("red")
+  for counter in range (20):
+    forward (150)
+    right(100)
+
+demo()
