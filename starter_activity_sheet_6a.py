26ea5bd71cdb7d360c0e3aecbba49485c6ced6b7
Lee Macri <lem@rathkeale.school.nz>
2022-02-24 12:56:05 +1300

    Create starter_activity_sheet_6a.py

---
+++ starter_activity_sheet_6a.py
@@ -0,0 +1,12 @@
+from turtle import *
+
+## What would you expect the computer to do? Write the output exactly as you think it will appear.
+
+def starter():
+  number = 0
+  while number <= 5:
+    print ("Hello")
+    number = number + 1
+  print ("Goodbye")
+
+starter()
